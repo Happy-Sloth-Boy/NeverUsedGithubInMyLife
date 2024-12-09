33685072c55760201be1086a7980d32457a071a7
Happy-Sloth-Boy <71262061+Happy-Sloth-Boy@users.noreply.github.com>
2024-12-09 18:54:03 +0000

    temp

---
+++ template.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 08:12:27 2024
+
+@author: RB0F337L
+"""
+import os
+
+
+cwd = os.getcwd()
+wd = r'X:\Winpython.385\AoC'
+
+excercise = 9
+
+case_file = os.path.join(wd,'in_e' + str(excercise) + '.txt')
+test_file = os.path.join(wd,'in_test_e' + str(excercise) + '.txt')
+
+'''
+FUNCTIONS HERE
+'''
+
+def treat_file():
+    file_id = j//2
+    file_size = LAYOUT_STR(j)
+    
+    return file_id(i + i+1 + ... + i+(file_size-1))
+
+def process_layout():
+    
+    total_sum = 0
+    i_block_position = 0
+    j_layout_position = 0
+    while # still have to move items :
+        
+        if j%2 == 0 :
+            treat_file(i,j)
+        if j%2 == 1 :
+            treat_space
+        
+    
+
+if __name__ == "__main__" :
+    file_path = case_file
+    #file_path = test_file
+    
+    '''
+    CODE HERE
+    '''
+    
+    print(ans_1)
+    print(ans_2)
