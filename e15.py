131337bc15f6454fa53f6ae203a1fca8b8b2bfa9
Happy-Sloth-Boy <71262061+Happy-Sloth-Boy@users.noreply.github.com>
2024-12-22 23:04:15 +0000

    Still ongoning

--- e15.py
+++ e15.py
@@ -15,63 +15,167 @@ Created on Thu Dec 19 10:07:21 2024
 import os
 
 cwd = os.getcwd()
-wd = r'X:\Winpython.385\AoC'
+wd = r'C:\Users\rbarr\Documents\AoC'
 
 excercise = 15
+tc = 'a'
 
 global CASE_FILE, TEST_FILE
 CASE_FILE = os.path.join(wd,'in_e' + str(excercise) + '.txt')
-TEST_FILE = os.path.join(wd,'in_test_e' + str(excercise) + '.txt')
+TEST_FILE = os.path.join(wd,'in_test_e' + str(excercise) + tc + '.txt')
 
-global MOVABLE_ROCK_LIST, UNMOVABLE_OBJECTS, ROBOT_POSITION, MAP_LAST_CHANGE, MOVE_INSTRUCTIONS, COOKIE_JAR
-global H_MAP, L_MAP
+global ROCK_DICT, MOVE_INSTRUCTIONS
+global ROBOT_POSITION, H_MAP, L_MAP
+global MAP_LIST, MAP_OBJ_LOC
+global DELTA_DICT
+global DEBUG_FLAG
+
+
 '''
-MOVABLE_ROCK_LIST = {i: (x,y)} for each rock i
-UNMOVABLE_OBJECTS = {i: (x,y)} for each object i
+ROCK_DICT = {i: (x,y)} for each rock i
 ROBOT_POSITION = (x,y)
-MAP_LAST_CHANGE = int
 MOVE_INSTRUCTIONS = '>^v<...'
-COOKIE_JAR = { str(pos)+"U/D/L/R": move_number }
 '''
 
 '''
 FUNCTIONS HERE
 '''
+def init_global_variables(map_list, H, L, rock_dict, move_inst) :
+    global MAP_LIST, H_MAP, L_MAP, MAP_OBJ_LOC
+    global ROCK_DICT, MOVE_INSTRUCTIONS, DELTA_DICT, DEBUG_FLAG
+    
+    ROCK_DICT = rock_dict
+    MOVE_INSTRUCTIONS = move_inst
 
-def pos_from_xy(x: int, y: int) -> :
-    return y*H_MAP + x
+    H_MAP = H
+    L_MAP = L
+    MAP_LIST = map_list
+    MAP_OBJ_LOC = [ [ '.' for x in range(L_MAP) ] for y in range(H_MAP) ]
+    for r, (x,y) in rock_dict.items(): MAP_OBJ_LOC[y][x] = r
+    
+    DELTA_DICT = {  '>' : (+1, 0) ,
+                    '<' : (-1, 0) ,
+                    'v' : ( 0,+1) ,
+                    '^' : ( 0,-1) }
+    DEBUG_FLAG = False
+    
+    
 
 def process_file(in_file: str)  :
-    global MACHINES_LIST
-    MACHINES_LIST = []
+    global ROBOT_POSITION
+    
+    rock_dictionary = {}
+    map_list = []
+    move_inst = ''
+    
+    read_map = True
     with open(in_file, 'r') as f:
-        for line in f:
+        for y,line in enumerate(f):
             
+            if read_map :
+                row = line.rstrip()
+                if row == '' :
+                    read_map = False
+                    L = x+1
+                    H = y
+                    continue
                 
+                map_list.append(list(row))
+                for x, el in enumerate(row) :
+                    if el == 'O' :
+                        rock_dictionary[len(rock_dictionary)] = (x,y)
+                    if el == '@' :
+                        ROBOT_POSITION = (x,y)
+
+            else :
+                move_inst += line.rstrip()
+    
+    init_global_variables(map_list,H,L,rock_dictionary,move_inst)
+                
+def pos_from_xy(x: int, y: int) -> int:
+    return y*H_MAP + x
+
+def get_GPS_coordinates(x,y) :
+    return y*100 + x
+
+def traspose_map(map_var: list, H: int, L: int) -> list:
+    return [ [ map_var[y][x] for y in range(H) ] for x in range(L) ]
+
+def print_map(map_var: list):
+    for row in map_var : print(''.join([str(el) for el in row]))
+
+def update_global_variables(r, sym, xo, yo, xn, yn):
+    MAP_LIST[yo][xo] = '.'
+    MAP_OBJ_LOC[yo][xo] = '.'
+    
+    MAP_LIST[yn][xn] = sym
+    MAP_OBJ_LOC[yn][xn] = r
+
+    if r != '.' :
+        ROCK_DICT[r] = (xn,yn)
+    
+    if sym == '@' :
+        global ROBOT_POSITION
+        ROBOT_POSITION = (xn, yn)
+
+def make_move(ins):
+
+    x , y  = ROBOT_POSITION
+    xn, yn = x , y
+    
+    dx, dy = DELTA_DICT[ins]
+
+    move_list = []
+    while True:
+        xn += dx
+        yn += dy
+        obj = MAP_OBJ_LOC[yn][xn]
+        if type(obj) != int :
+            break
+        move_list.insert(0,obj)
+    
+    if MAP_LIST[yn][xn] == '#': 
+        return
+
+    for rock in move_list :
+        x_old, y_old = ROCK_DICT[rock]
+        x_new, y_new = x_old+dx, y_old+dy
+        update_global_variables(rock, 'O', x_old, y_old, x_new, y_new)
+
+    update_global_variables('.', '@', x, y, x+dx, y+dy)
 
 def execute_instructions():
-    global MOVE_INSTRUCTIONS
-    for n, instruction in enumerate(MOVE_INSTRUCTIONS): 
+    
+    for N, instruction in enumerate(MOVE_INSTRUCTIONS): 
+        x , y = ROBOT_POSITION
         
-        cookie = str(pos_from_xy(x, y)) + instruction
-        in_loop = COOKIE_JAR[cookie] > MAP_LAST_CHANGE if cookie in COOKIE_JAR else False
-        if in_loop :
-            break
-        
-        COOKIE_JAR[cookie] = n
         make_move(instruction)
         
-        
+        if DEBUG_FLAG: 
+            print(f"\nMove number {N+1}: {instruction}")
+            print_map(MAP_LIST)
 
+def get_total_GPS():
+    total_GPS = 0
+    for x,y in ROCK_DICT.values():
+        total_GPS += get_GPS_coordinates(x,y)
+    return total_GPS
 
 if __name__ == "__main__" :
     file_path = CASE_FILE
     #file_path = TEST_FILE
     
+    process_file(file_path)
+
+    if DEBUG_FLAG:
+        print(f"\nInitial State:")
+        print_map(MAP_LIST)
     
+    execute_instructions()
     
-    ans_1 = total_tokens_1
-    ans_2 = total_tokens_2
+    ans_1 = get_total_GPS()
+    #ans_2 = total_tokens_2
     
     print(ans_1)
-    print(ans_2)
+    #print(ans_2)
+    
